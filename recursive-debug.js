a551e52614ffcfaefa749ca5c190a941fcfddb63
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-05-26 18:51:51 -0400

    Debug doForAll

--- recursive-debug.js
+++ recursive-debug.js
@@ -13,8 +13,10 @@ doForAll([1, 2, 3], (x) => x + 1); // => [2, 3, 4]
 doForAll(["a", "b", "c"]), (x) => x.toUpperCase()); // => ["A", "B", "C"]
 ***********************************************************************/
 
-function doForAll(arr, action) {
-  return [action(arr[0]), ...doForAll(arr.slice(1), action)];
+function doForAll(arr, action, done = []) {
+  if (!arr.length) return done;
+  done.push(action(arr[0]));
+  return doForAll(arr.slice(1), action, done);
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
